fef689843201b9e15ce8ccc28c3180738f19fe19
LikeDrinkingWater_glb <LikeDrinkingWater_glb@chl.com>
2019-07-12 11:31:58 +0800

    update

--- string/README.md
+++ string/README.md
@@ -1,3 +1,50 @@
 # 字符串
 ## 目录简介
 * [matchSubstring](https://github.com/Like-Drinking-water/algorithms/tree/master/string/matchSubstring) - 匹配子串
+## KMP算法
+通过i, j分别标记主串跟子串匹配的当前位置位置  
+先通过子串来判断当某个位置失配时  
+j 应该标记的子串的什么地方
+``` c
+//首先先获取 next , 即失配时 j 应移动的位置
+i = 1; j = 0;
+next[1] = 0;
+//t[0] 保存字符串的长度
+while (i < t[0]) {
+	if (j == 0 || t[i] == t[j]) {
+		i++;
+		j++;
+		//此时表示在 i 位置出失配了
+		//则应该移动到 j 位置上
+		//因为 1 ～ j - 1 位置和 i - j + 1 ～ i - 1 位置上的字符相同
+		//无需重新匹配
+		next[i] = j;
+	}
+	else {
+		//不匹配则回到上一处进行匹配
+		j = next[j];
+	}
+}
+```
+获取完 next 后则可以根据 next 来完成算法
+``` c
+//pos 表示开始匹配的位置
+i = pos; j = 1;
+while (i <= s[0] && j <= t[0]) {
+	if (s[i] == t[j] || j == 0) {
+		i++;
+		j++;
+	}
+	else {
+		j = next[j];
+	}
+}
+//表示匹配成功
+if (j > t[0]) {
+	//返回开始匹配的位置
+	return i - t[0];
+}
+else {
+	return -1;
+}
+```
---
+++ string/matchSubstring/matchSubstring-KMP-c/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**************************************
+问题：字符串匹配 
+***************************************/
+
+#define MAXSTRLEN 255
+
+typedef unsigned char SString[MAXSTRLEN + 1];
+
+int next[MAXSTRLEN + 1];
+
+//获取next 
+void getNext(SString s, int next[]) {
+	int i, j;
+	i = 1;
+	j = 0;
+	next[1] = 0;
+	while (i < s[0]) {
+		if (j == 0 || s[i] == s[j]) {
+			i++;
+			j++;
+			next[i] = j;
+		}
+		else {
+			j = next[j]; 
+		}
+	}
+}
+
+//匹配失败返回 -1； 匹配到返回开始匹配位置 
+int Index(SString s, SString t, int pos) {
+	int i, j;
+	i = pos;
+	j = 1;
+	getNext(t, next);
+	while (i <= s[0] && j <= t[0]) {
+		if (j == 0 || s[i] == t[j]) {
+			i++;
+			j++;
+		}
+		else {
+			j = next[j];
+		}
+	}
+	if (j > t[0]) {
+		return i - t[0];
+	}
+	else {
+		return -1;
+	}
+}
+
+int main(void) {
+	SString s, t;
+	s[0] = 7; s[1] = '1'; s[2] = '0'; s[3] = '0'; s[4] = '0'; s[5] = '0'; s[6] = '1'; s[7] = '1';
+	t[0] = 3; t[1] = '0'; t[2] = '1'; t[3] = '1';
+	printf("%d", Index(s, t, 1));
+	return 0;
+}
